800d6a779bfe90b1ebb65e621f822c26e0085bbb
ian <ian.wen.2002@gmail.com>
2024-05-16 01:53:45 -0700

    added checks for input

--- bonusCalculator.js
+++ bonusCalculator.js
@@ -13,11 +13,23 @@ desiredPayQuota.addEventListener("input", (e) => {desiredPaycheck();})
 desiredPayDeadline.addEventListener("input", (e) => {desiredPaycheck();})
 
 function otBonus() {
+  if (!/^\d+$/.test(overtimeSold.value)) {
+    document.getElementById("overtime-bonus").innerHTML = "enter value for scraps sold";
+    return;
+  }
+  if (!/^\d+$/.test(overtimeQuota.value)) {
+    document.getElementById("overtime-bonus").innerHTML = "enter quota";
+    return;
+  }
+  if (!/^\d+$/.test(overtimeDeadline.value)) {
+    document.getElementById("overtime-bonus").innerHTML = "enter days until deadline";
+    return;
+  }
   let sold = Number(overtimeSold.value);
   let quota = Number(overtimeQuota.value);
   let deadline = Number(overtimeDeadline.value);
   if (sold < quota) {
-    document.getElementById("overtime-bonus").innerHTML = "you get 0 credits of overtime bonus if you sold " + sold.toString() + " credits worth of scraps when the quota is " + quota.toString() + " credits, " + deadline.toString() + " day(s) before the deadline";
+    document.getElementById("overtime-bonus").innerHTML = "you get <b>0</b> credits of overtime bonus if you sold " + sold.toString() + " credits worth of scraps when the quota is " + quota.toString() + " credits, " + deadline.toString() + " day(s) before the deadline";
     return;
   }
   if (sold < 0 || quota < 0 || deadline < 0) {
@@ -43,15 +55,27 @@ function otBonus() {
       break;
   }
   let ot = (sold - quota) / 5 + extra;
-  document.getElementById("overtime-bonus").innerHTML = "you get <b>" + ((ot > 0) ? Math.floor(ot).toString() : "0") + "</b> credits of overtime bonus if you sold " + sold.toString() + " credits worth of scraps when the quota is " + quota.toString() + "credits, " + deadline.toString() + " day(s) before the deadline";
+  document.getElementById("overtime-bonus").innerHTML = "you get <b>" + ((ot > 0) ? Math.floor(ot).toString() : "0") + "</b> credits of overtime bonus if you sold " + sold.toString() + " credits worth of scraps when the quota is " + quota.toString() + " credits, " + deadline.toString() + " day(s) before the deadline";
 }
 
 function desiredPaycheck() {
+  if (!/^\d+$/.test(desiredPay.value)) {
+    document.getElementById("desired-paycheck-sell").innerHTML = "enter desired paycheck";
+    return;
+  }
+  if (!/^\d+$/.test(desiredPayQuota.value)) {
+    document.getElementById("desired-paycheck-sell").innerHTML = "enter quota";
+    return;
+  }
+  if (!/^\d+$/.test(desiredPayDeadline.value)) {
+    document.getElementById("desired-paycheck-sell").innerHTML = "enter days until deadline";
+    return;
+  }
   let pay = Number(desiredPay.value);
   let quota = Number(desiredPayQuota.value);
   let deadline = Number(desiredPayDeadline.value);
   if (pay <= quota) {
-    document.getElementById("desired-paycheck-sell").innerHTML = "you should sell " + pay.toString() + " credits worth of scraps if you want " + pay.toString() + " credits of total paycheck when the quota is " + quota.toString() + "credits, " + deadline.toString() + " day(s) before the deadline";
+    document.getElementById("desired-paycheck-sell").innerHTML = "you should sell <b>" + pay.toString() + "</b> credits worth of scraps if you want " + pay.toString() + " credits of total paycheck when the quota is " + quota.toString() + " credits, " + deadline.toString() + " day(s) before the deadline";
     return;
   }
   if (pay < 0 || quota < 0 || deadline < 0) {
@@ -77,5 +101,5 @@ function desiredPaycheck() {
       break;
   }
   let sell = ((pay - extra) * 5 + quota) / 6
-  document.getElementById("desired-paycheck-sell").innerHTML = "you should sell <b>" + Math.ceil(sell).toString() + "</b> credits worth of scraps if you want " + pay.toString() + " credits of total paycheck when the quota is " + quota.toString() + " credits, " + deadline.toString() + " day(s) before the deadline";
+  document.getElementById("desired-paycheck-sell").innerHTML = "you should sell <b>" + ((sell > 0) ? Math.ceil(sell).toString() : quota.toString()) + "</b> credits worth of scraps if you want " + pay.toString() + " credits of total paycheck when the quota is " + quota.toString() + " credits, " + deadline.toString() + " day(s) before the deadline";
 }
